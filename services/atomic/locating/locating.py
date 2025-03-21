a19de9efa72f4ae222203319cb3ae2356642a781
WongYH <wyhui@outlook.com>
2025-03-22 02:56:46 +0800

    Changed Locating to GRPC

--- services/atomic/locating/locating.py
+++ services/atomic/locating/locating.py
@@ -1,112 +1,53 @@
-from invokes import invoke_http
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
+from concurrent import futures #Allow to set no. of workers on server
+import grpc
+import locate_pb2
+import locate_pb2_grpc
+
 import extra_functions
 
-app = FastAPI(
-    title="Locating Atomic Microservice",
-    description="Service for finding center point and closest volunteers"
-)
+class LocateService(locate_pb2_grpc.locateServicer):
+    def getFilteredUsers(self, request, context):
+        print("Received Request")
+        product_id = request.productId
+        product_address = request.productAddress
+        volunteer_list = request.volunteerList
 
-# CORS so front end can access
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="*",
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+        product_coord = extra_functions.convertAddress(product_address)
+        if product_coord is None:
+            return locate_pb2.responseBody(
+                productId=product_id,
+                productClosestCC=None,
+                userList=[],
+                error="Not a valid address!"
+            )
+        
+        closest_cc = extra_functions.find_closest_cc(product_coord)
+        if closest_cc is None:
+            return locate_pb2.responseBody(
+                productId=product_id,
+                productClosestCC=None,
+                userList=[],
+                error="Can't find closest CC!"
+            )
+        
+        center_point_coord = extra_functions.get_center_point(product_coord,closest_cc["coordinates"])
+        volunteer_list = volunteer_list
+        filtered_closest_list = extra_functions.find_closest_users(center_point_coord,volunteer_list)
 
-class volunteerInfo(BaseModel):
-    userId:str
-    userAddress:str
-
-class inputBody(BaseModel):
-    productId: str
-    productAddress: str
-    volunteerList: list[volunteerInfo] = Field(...,description="List of volunteers with their information")
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "productId": "prod_789012",
-                "productAddress": "B1-67 SMU School of Computing and Information Systems 1, Singapore 178902",
-                "volunteerList": [
-                    {
-                        "userId": "1111-1111-1111",
-                        "userAddress": "80 Stamford Rd, Singapore 178902"
-                    },
-                    {
-                        "userId": "2222-2222-2222",
-                        "userAddress": "501 Margaret Dr, Singapore 149306"
-                    },
-                                        {
-                        "userId": "3333-3333-3333",
-                        "userAddress": "500 Dover Rd, Singapore 139651"
-                    }
-                ]
-            }
-        }
-
-class responseBody(BaseModel):
-    productId:str
-    productClosestCC:str | None
-    userList:list[str] = Field(...,description="List of filtered volunteers")
-    error: str | None
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "productId": "prod_789012",
-                "productClosestCC": "Pek Kio Community Centre|21 Gloucester Rd, Singapore 219458",
-                "userList": [
-                    "1111-1111-1111"
-                ]
-            }
-        }
-
-@app.get("/")
-async def my_first_get_api():
-    return {"message":"First FastAPI example"}
-
-@app.post("/closest/", response_model=responseBody)
-async def find_closest_users(input:inputBody):
-    res = None
-    product_address = input.productAddress
-
-    product_coord = extra_functions.convertAddress(product_address)
-    if product_coord is None:
-        return responseBody(
-            productId=input.productId,
-            productClosestCC=None,
-            userList=[],
-            error="Not a valid address!"
+        reponse = locate_pb2.responseBody(
+            productId = product_id,
+            productClosestCC = f"{closest_cc['display_name']}|{closest_cc['address']}",
+            userList = filtered_closest_list,
+            error = None
         )
+        return reponse
 
-    closest_cc = extra_functions.find_closest_cc(product_coord)
-    if closest_cc is None:
-        return responseBody(
-            productId=input.productId,
-            productClosestCC=None,
-            userList=[],
-            error="Can't find closest CC!"
-        )
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    locate_pb2_grpc.add_locateServicer_to_server(LocateService(), server)
+    server.add_insecure_port("0.0.0.0:5006")
+    server.start()
+    server.wait_for_termination()
 
-
-    center_point_coord = extra_functions.get_center_point(product_coord,closest_cc["coordinates"])
-
-    volunteer_list = input.volunteerList
-    filtered_closest_list = extra_functions.find_closest_users(center_point_coord,volunteer_list)
-
-    res = responseBody(
-        productId = input.productId,
-        productClosestCC = f"{closest_cc['display_name']}|{closest_cc['address']}",
-        userList = filtered_closest_list,
-        error = None
-    )
-
-    return res
-
-# taskkill /f /im python.exe in powershell to force kill it
-# python -m uvicorn locating:app --reload --port 5003
+if __name__ == '__main__':
+    serve()
