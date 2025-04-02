7ba9e70d91ed528c234f1dfce183cf5fad27f3ac
WongYH01 <wyhui@outlook.com>
2025-04-02 21:45:51 +0800

    enhanced product validation

--- services/atomic/product_validation/jibiti.py
+++ services/atomic/product_validation/jibiti.py
@@ -3,6 +3,7 @@ import base64
 import json
 from dotenv import load_dotenv
 from openai import OpenAI 
+from invokes import invoke_http
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_KEY")
@@ -28,12 +29,37 @@ reply_schema = {
     "strict": True
 }
 
+reply_schema_edited = {
+    "format": {
+        "type": "json_schema",
+        "name":"image_validation",
+        "schema": {
+            "type": "object",
+            "properties":{
+                "result":{
+                    "description":"Result after the analysis",
+                    "type":["boolean", "string"]
+                }
+            },
+            "required": ["result"],
+            "additionalProperties": False
+        },
+        "strict": True
+    }
+}
+
 system_content = open('system_message.txt', 'r').read()
 
 # Cause OpenAI api only accept base64 string in utf-8 format
 def encode_image(image):
     res = base64.b64encode(image.read()).decode("utf-8")
     return res
+
+def encode_image_path(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
 
 
 def call_openai(image, description):
@@ -52,7 +78,7 @@ def call_openai(image, description):
             {
                 "role":"user",
                 "content":[
-                    { "type": "text", "text": f"Check that the image contains ALL of the following: {all_items}" },
+                    { "type": "text", "text": f"{all_items}" },
                     {
                         "type": "image_url",
                         "image_url": {
@@ -86,3 +112,56 @@ def call_openai(image, description):
         print(f"Error in call_openai: {str(e)}")
         # Return a structured error that can be handled by the calling function
         return json.dumps({"error": f"Failed to process with OpenAI: {str(e)}"})
+
+
+def newcall_openai(image, description):
+    base64_image = encode_image(image)
+    all_items = ",".join(description)
+    print(all_items)
+    response = client.responses.create(
+        model="gpt-4o",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    { 
+                        "type": "input_text", 
+                        "text": f"{all_items}"
+                    },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            },
+            {
+                "role": "system",
+                "content":[
+                    {
+                        "type":"input_text",
+                        "text": system_content
+                    }
+                ]
+            },
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                    "type": "output_text",
+                    "text": "Please ensure all are non-perishable and the picture contains all items stated"
+                    }
+                ]
+            }
+        ],
+        text = reply_schema_edited,
+        reasoning={},
+        tools=[],
+        temperature=0.3,
+        max_output_tokens=2048,
+        top_p=1,
+        store=True
+    )
+    result = response.output[0].content[0].text
+    # result = json.loads(result)
+    return result
+
